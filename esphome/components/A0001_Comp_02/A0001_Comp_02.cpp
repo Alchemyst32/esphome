b913f57fefca4342070f738dadd9d9b502d4ca07
Alchemyst32 <alex.b@teslein.com>
2024-08-29 10:09:23 +0200

    q

--- esphome/components/A0001_Comp_02/A0001_Comp_02.cpp
+++ esphome/components/A0001_Comp_02/A0001_Comp_02.cpp
@@ -134,7 +134,7 @@ void A0001_Comp_02Component::loop() {
   while (available()) {
     this->readline_(read(), buffer, max_line_length);
     //ESP_LOGV(TAG, "HELLO WORLD!"); //ALEX
-    //this->set_Gpio_Config();
+    this->set_Gpio_Config();
   }
 }
 
