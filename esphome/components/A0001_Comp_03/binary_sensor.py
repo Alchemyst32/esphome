ec6d3b5287c50fbbb6f379d63886793c9a12e387
Alchemyst32 <alex.b@teslein.com>
2024-08-30 09:24:48 +0200

    d

--- esphome/components/A0001_Comp_03/binary_sensor.py
+++ esphome/components/A0001_Comp_03/binary_sensor.py
@@ -5,6 +5,7 @@ from esphome.const import (
     DEVICE_CLASS_MOTION,
     DEVICE_CLASS_OCCUPANCY,
     DEVICE_CLASS_PRESENCE,
+    DEVICE_CLASS_SWITCH, #ALEX /////////////////////////////////////////////////////////////////////////////////////////////////
     ENTITY_CATEGORY_DIAGNOSTIC,
     ICON_MOTION_SENSOR,
     ICON_ACCOUNT,
@@ -16,6 +17,8 @@ from . import CONF_A0001_Comp_03_ID, A0001_Comp_03Component
 
 DEPENDENCIES = ["A0001_Comp_03"]
 CONF_OUT_PIN_PRESENCE_STATUS = "out_pin_presence_status"
+
+CONF_OUT_ALEX_SIGNAL_STATUS = "out_pin_Alex_Signal_status" #ALEX//////////
 
 CONFIG_SCHEMA = {
     cv.GenerateID(CONF_A0001_Comp_03_ID): cv.use_id(A0001_Comp_03Component),
@@ -36,6 +39,13 @@ CONFIG_SCHEMA = {
         entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
         icon=ICON_ACCOUNT,
     ),
+    #ALEX/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    cv.Optional(CONF_OUT_ALEX_SIGNAL_STATUS): binary_sensor.binary_sensor_schema(
+        device_class=DEVICE_CLASS_SWITCH,
+        entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
+        icon=ICON_ACCOUNT,
+    ),
+    #ALEX/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
 
 
@@ -53,3 +63,9 @@ async def to_code(config):
     if out_pin_presence_status_config := config.get(CONF_OUT_PIN_PRESENCE_STATUS):
         sens = await binary_sensor.new_binary_sensor(out_pin_presence_status_config)
         cg.add(A0001_Comp_03_component.set_out_pin_presence_status_binary_sensor(sens))
+    #ALEX/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    if out_pin_Alex_Signal_status_config := config.get(CONF_OUT_ALEX_SIGNAL_STATUS):
+        sens = await binary_sensor.new_binary_sensor(out_pin_Alex_Signal_status_config)
+        cg.add(A0001_Comp_03_component.set_out_pin_presence_status_binary_sensor(sens))
+
+    #ALEX/////////////////////////////////////////////////////////////////////////////////////////////////////////////
